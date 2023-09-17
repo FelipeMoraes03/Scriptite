efe660d0a481a13f15d4e40a4d263bd699e01ef0
jussararodrigues <jussara.rodrigues10@hotmail.com>
2023-09-17 15:11:56 -0300

    refactor: corrige exibição criativo (protótipo)

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,71 +1,72 @@
-import {useState} from 'react';
-import "./header.css";
+import { useState } from 'react'
+import './header.css'
+import { FaCheckCircle } from 'react-icons/fa'
 
-function Header({screen}){
+function Header({ screen }) {
+  let b1 = '',
+    b2 = '',
+    b3 = '',
+    b4 = '',
+    i1 = '',
+    i2 = '',
+    i3 = '',
+    i4 = ''
+  switch (screen) {
+    case 1:
+      b1 = 'selectBorder'
+      break
+    case 2:
+      b2 = 'selectBorder'
+      i1 = 'finished'
+      break
+    case 3:
+      b3 = 'selectBorder'
+      i1 = 'finished'
+      i2 = 'finished'
+      break
+    case 4:
+      b4 = 'selectBorder'
+      i1 = 'finished'
+      i2 = 'finished'
+      i3 = 'finished'
+      break
 
-        let b1='',b2='',b3='',b4='';
-        switch (screen) {
-                case 1: b1='selectBorder';break;
-                case 2: b2='selectBorder';break;
-                case 3: b3='selectBorder';break;
-                case 4: b4='selectBorder';break;
-        
-                default: break;
-        }
+    default:
+      break
+  }
 
-    return(
-        <header id="header">
-  
-                  <div className="logoBox headerBoxLogo bord">
-                      <h1 className="fontColor2 fontSize1">
-                          script
-                      </h1>
-                      <h1 className="fontColor3 fontSize1">
-                          ite
-                      </h1>  
-                  </div>
-  
-                  <div className={'headerBoxPage ' + b1}>
-                    <div>
-                            Etapa 1
-                    </div>
-                    <div>
-                            Geração de criativos
-                    </div>
-  
-                  </div>
-                  <div className={'headerBoxPage ' + b2}>
-                  <div>
-                            Etapa 2
-                    </div>
-                    <div>
-                           Geração de roteiro
-                    </div>
-                      
-                  </div>
-                  <div className={'headerBoxPage ' + b3}>
+  return (
+    <header id="header">
+      <div className="logoBox headerBoxLogo bord">
+        <h1 className="fontColor2 fontSize1">script</h1>
+        <h1 className="fontColor3 fontSize1">ite</h1>
+      </div>
 
-                  <div>
-                            Etapa 3
-                    </div>
-                    <div>
-                            Geração de storyboard
-                    </div>
-                      
-                  </div>
-                  <div className={'headerBoxPage ' + b4}>
-
-                  <div>
-                            Etapa 4
-                    </div>
-                    
-                    <div>
-                            Visualização final
-                    </div>
-                      
-                  </div>
-  
-        </header>
-    );
-
-  } export default Header;
+      <div className={'headerBoxPage ' + b1 + '' + i1}>
+        <div>
+          Etapa 1 <FaCheckCircle />
+        </div>
+        <div>Geração de criativo</div>
+      </div>
+      <div className={'headerBoxPage ' + b2 + '' + i2}>
+        <div>
+          Etapa 2 <FaCheckCircle />
+        </div>
+        <div>Geração de roteiro</div>
+      </div>
+      <div className={'headerBoxPage ' + b3 + '' + i3}>
+        <div>
+          Etapa 3 <FaCheckCircle />
+        </div>
+        <div>Geração de storyboard</div>
+      </div>
+      <div className={'headerBoxPage ' + b4 + '' + i4}>
+        <div>
+          Etapa 4 <FaCheckCircle />
+        </div>
+        <div>Visualização final</div>
+      </div>
+    </header>
+  )
+}
+export default Header
