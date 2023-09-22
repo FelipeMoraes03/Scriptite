e851f4951c9beee06b8271d3eddc1e2de66f4f83
jvras58 <jvras@cin.ufpe.br>
2023-09-22 13:19:09 -0300

    wp: tela geradora do script v1

--- src/components/generateScript/generateScript.js
+++ src/components/generateScript/generateScript.js
@@ -19,158 +19,153 @@ setCreative(novasPartes.join('<br /><br />'));
 }
 
 const camposDeTexto = creative.split('<br /><br />').map((parte, index) => {
-    const [rotulo, conteudo] = parte.split(': ');
-  
-    if (rotulo === "Criativo 1") {
-      return(
-        <>
-        <span className="inputBox">{rotulo}</span>
-        </>
-      );
-    }
-  
-    return (
-      <div key={index}>
-        {rotulo && <p><strong className="inputBox">{rotulo}:</strong></p>}
-        <InputField
-          label={rotulo ? "" : `Criativo selecionado:`}
-          value={conteudo || parte}
-          setValue={novaParte => atualizarParte(novaParte, index)}
-        />
-      </div>
-    );
-  });
-  
-return (
-    <div>
-        <Header screen={2} />
-    
-        <div className="bodyCreative backgroundColor1">
-        <div className="creativeBox obc11">
-          <div>
-            {camposDeTexto}
-            <ShowCreative creative={creative} setCreative={setCreative} hidden={true} />
-            <GenerateButton input={creative} setKeyWord={setCreative} setScript={setScript} text={"GERAR SCRIPT"} />
-          </div>
-        </div>
-        <div id="icon" className="fontColor2">
-            <FaArrowRight />
-        </div>
-        <div className="creativeBox obc11" id="outputBoxCreative">
-            <ShowScript text={script} />
-            <StoryBoardPageButton creative={creative} script={script} text={"PRÓXIMO"}/>
-        </div>    
-        </div>
-        <div className=" footer fontColor4">
-        Copyright © 2023 | Todos os direitos reservados
-        </div>
-    </div>
-    );
-    }
+  const [rotulo, conteudo] = parte.split(': ');
 
-function InputField(props) {
-    return (
-      <div className="inputBox">
-        <num>{props.label}</num>
-        {props.value === "Criativo 1" ? (
-          <span className="inputBox">{props.value}</span>
-        ) : (
-          <span className="input">{props.value}</span>
-        )}
-      </div>
+  if (rotulo === "Criativo 1") {
+    return(
+      <>
+      <span className="inputBox">{rotulo}</span>
+      </>
     );
   }
+
+  return (
+    <div key={index}>
+      {rotulo && <p><strong className="inputBox">{rotulo}:</strong></p>}
+      <InputField
+        label={rotulo ? "" : `Criativo selecionado:`}
+        value={conteudo || parte}
+        setValue={novaParte => atualizarParte(novaParte, index)}
+      />
+    </div>
+  );
+});
+
+return (
+  <div>
+      <Header screen={2} />
   
-  
+      <div className="bodyCreative backgroundColor1">
+      <div className="creativeBox obc11">
+        <div>
+          {camposDeTexto}
+          <ShowCreative creative={creative} setCreative={setCreative} hidden={true} />
+          <GenerateButton input={creative} setKeyWord={setCreative} setScript={setScript} text={"GERAR SCRIPT"} />
+        </div>
+      </div>
+      <div id="icon" className="fontColor2">
+          <FaArrowRight />
+      </div>
+      <div className="creativeBox obc11" id="outputBoxCreative">
+          <ShowScript text={script} />
+          <StoryBoardPageButton creative={creative} script={script} text={"PRÓXIMO"}/>
+      </div>    
+      </div>
+      <div className=" footer fontColor4">
+      Copyright © 2023 | Todos os direitos reservados
+      </div>
+  </div>
+  );
+  }
+
+function InputField(props) {
+  return (
+    <div className="inputBox">
+      <num>{props.label}</num>
+      {props.value === "Criativo 1" ? (
+        <span className="inputBox">{props.value}</span>
+      ) : (
+        <span className="input">{props.value}</span>
+      )}
+    </div>
+  );
+}
+
+
 export default Main;
 
 function GenerateButton(props) {  
 let updatedScript = ""
 async function generateScriptClick() {
 try {
-    let prompt = promptScript;
-    prompt = prompt + props.input;
+  let prompt = promptScript;
+  prompt = prompt + props.input;
 
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {"role": "user", "content": prompt}
-        ],
-        stream: true,
-    });
-    
-        for await (const chunk of completion) {
-        if (chunk.choices[0].delta.content) {
-            updatedScript = updatedScript + chunk.choices[0].delta.content
-            const formattedScript = updatedScript.replace(/\n/g, '<br />')
-            props.setScript(formattedScript)
-        }
-        }
+  const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+          {"role": "user", "content": prompt}
+      ],
+      stream: true,
+  });
+  
+      for await (const chunk of completion) {
+      if (chunk.choices[0].delta.content) {
+          updatedScript = updatedScript + chunk.choices[0].delta.content
+          const formattedScript = updatedScript.replace(/\n/g, '<br />')
+          props.setScript(formattedScript)
+      }
+      }
 
 } catch (err) {
-    console.error(err);
-    alert(err);
+  console.error(err);
+  alert(err);
 }
 }
 
 return (
 <generate>
-    <button className='creaButton' onClick={generateScriptClick}>{props.text}</button>
-    <div>{props.creative}</div>
+  <button className='creaButton' onClick={generateScriptClick}>{props.text}</button>
+  <div>{props.creative}</div>
 </generate>
 );
 }
 
 function ShowCreative(props) {
-    const location = useLocation();
-    const infos = location.state?.infos;
-    const creative = infos[0];
-    props.setCreative(creative);
-  
-    return (
-      <p className='showCreative' style={{ display: props.hidden ? 'none' : 'block' }} dangerouslySetInnerHTML={{ __html: creative }}></p>
-    );
-  }
-  
+  const location = useLocation();
+  const infos = location.state?.infos;
+  const creative = infos[0];
+  props.setCreative(creative);
+
+  return (
+    <p className='showCreative' style={{ display: props.hidden ? 'none' : 'block' }} dangerouslySetInnerHTML={{ __html: creative }}></p>
+  );
+}
 
 
-  function ShowScript(props) {
-    const script = props.text;
-    const scriptLines = script.split('<br />');
-    const paragraphs = [];
-  
-    scriptLines.forEach(line => {
-      const colonIndex = line.indexOf(':');
-      if (colonIndex !== -1) {
-        const key = line.slice(0, colonIndex);
-        const value = line.slice(colonIndex + 1).trim();
-        console.log(key, value);
-        if (key.localeCompare('Roteiro do criativo') === 0) {
-          paragraphs.push(
-            <div id="identifierCreative" key={key}>
-              <p className="title">{key}</p>
-              <hr />
-            </div>
-          );
-        } else {
-          paragraphs.push(
-            <div className="responseItem" key={key}>
-              <label className="title">{key}:</label>
-              {value && <p className="value input">{value}</p>}
-            </div>
-          );
-        }
+
+function ShowScript(props) {
+  const script = props.text;
+  const scriptLines = script.split('<br />');
+  const paragraphs = [];
+
+  scriptLines.forEach(line => {
+    const colonIndex = line.indexOf(':');
+    if (colonIndex !== -1) {
+      const key = line.slice(0, colonIndex);
+      const value = line.slice(colonIndex + 1).trim();
+      if (key.localeCompare('Roteiro do criativo') === 0) {
+        paragraphs.push(
+          <div id="identifierCreative" key={key}>
+            <p className="title">{key}</p>
+            <hr />
+          </div>
+        );
+      } else {
+        paragraphs.push(
+          <div className="responseItem" key={key}>
+            <label className="title">{key}:</label>
+            {value && <p className="value input">{value}</p>}
+          </div>
+        );
       }
-    });
-  
-    return (
-      <div className="showCreative">{paragraphs}</div>
-    );
-  }
-  
+    }
+  });
 
-
-
+  return (
+    <div className="showCreative">{paragraphs}</div>
+  );
+}
 
 function StoryBoardPageButton(props) {
 const navigate = useNavigate();
@@ -182,17 +177,17 @@ navigate('/story-board', { state: { infos } });
 
 const storyBoardClick = async () => {
 if (props.script === "") {
-    alert("É necessário gerar um script antes de avançar");
+  alert("É necessário gerar um script antes de avançar");
 }
 }
 
 return (
 <next>
-    {props.script === "" ? (
-        <button className='creaButton' onClick={storyBoardClick}>{props.text}</button>
-    ) : (
-        <button className='creaButton' onClick={handleStoryBoardPage}>{props.text}</button>
-    )}
+  {props.script === "" ? (
+      <button className='creaButton' onClick={storyBoardClick}>{props.text}</button>
+  ) : (
+      <button className='creaButton' onClick={handleStoryBoardPage}>{props.text}</button>
+  )}
 </next>
 );
 }
