c74561065e23355bed37d9e212c083f0b7e30fc1
jvras58 <jvras@cin.ufpe.br>
2023-09-26 14:16:12 -0300

    wp: chave sendo solicitada ao cliente para garantir segurança

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -8,7 +8,7 @@ import img1 from './img1.png';
 function Home() {
 
     const handleClick = () => {
-        let key = prompt("Por favor, insira a chave da API");
+        let key = prompt("Por favor, insira a chave da API Openai Exemplo(sk-b3xXS...WYjR):");
         if (key) {
             localStorage.setItem('api', key);
             console.log(localStorage.getItem('api'))
